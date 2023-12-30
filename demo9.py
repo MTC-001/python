1c0cd1944c334389dc2c528bf9e7a069ef870fd2
MTC-001 <1135494966@qq.com>
2023-12-30 17:53:49 +0800

    12.30new

--- demo9.py
+++ demo9.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-DNA_SIZE = 24
-POP_SIZE = 200
-CROSSOVER_RATE = 0.8
-MUTATION_RATE = 0.005
-N_GENERATIONS = 50
-X_BOUND = [-3, 3]
-Y_BOUND = [-3, 3]
+DNA_SIZE = 24 #编码长度
+POP_SIZE = 200 #种群矩阵
+CROSSOVER_RATE = 0.8 #交叉概率
+MUTATION_RATE = 0.005 #突变概率
+N_GENERATIONS = 50 #迭代次数 
+X_BOUND = [-3, 3] #x取值范围
+Y_BOUND = [-3, 3] #y取值范围
 
-
+#函数
 def F(x, y):
 	return 3*(1-x)**2*np.exp(-(x**2)-(y+1)**2)- 10*(x/5 - x**3 - y**5)*np.exp(-x**2-y**2)- 1/3**np.exp(-(x+1)**2 - y**2)
 
+#画图
 def plot_3d(ax):
 
 	X = np.linspace(*X_BOUND, 100)
@@ -31,21 +32,26 @@ def plot_3d(ax):
 	plt.pause(3)
 	plt.show()
 
+#得到适应度
 def get_fitness(pop): 
     x,y = translateDNA(pop)
     pred = F(x, y)
     return (pred - np.min(pred)) + 1e-3 
 #减去最小的适应度是为了防止适应度出现负数，通过这一步fitness的范围为[0, np.max(pred)-np.min(pred)],最后在加上一个很小的数防止出现为0的适应度
 
+#解码
 def translateDNA(pop): #pop表示种群矩阵，一行表示一个二进制编码表示的DNA，矩阵的行数为种群数目
 	x_pop = pop[:,1::2]#奇数列表示X
 	y_pop = pop[:,::2] #偶数列表示y
 	
 	#pop:(POP_SIZE,DNA_SIZE)*(DNA_SIZE,1) --> (POP_SIZE,1)
+    #映射为x范围内的数字
 	x = x_pop.dot(2**np.arange(DNA_SIZE)[::-1])/float(2**DNA_SIZE-1)*(X_BOUND[1]-X_BOUND[0])+X_BOUND[0]
+    #映射为y范围内的数字
 	y = y_pop.dot(2**np.arange(DNA_SIZE)[::-1])/float(2**DNA_SIZE-1)*(Y_BOUND[1]-Y_BOUND[0])+Y_BOUND[0]
 	return x,y
 
+#交叉和变异
 def crossover_and_mutation(pop, CROSSOVER_RATE = 0.8):
 	new_pop = []
 	for father in pop:		#遍历种群中的每一个个体，将该个体作为父亲
@@ -62,9 +68,10 @@ def crossover_and_mutation(pop, CROSSOVER_RATE = 0.8):
 def mutation(child, MUTATION_RATE=0.003):
 	if np.random.rand() < MUTATION_RATE: 				#以MUTATION_RATE的概率进行变异
 		mutate_point = np.random.randint(0, DNA_SIZE*2)	#随机产生一个实数，代表要变异基因的位置
-		child[mutate_point] = child[mutate_point]^1 	#将变异点的二进制为反转
+		child[mutate_point] = child[mutate_point]^1 	#将变异点的二进制位反转
 
-def select(pop, fitness):    # nature selection wrt pop's fitness
+#根据适应度将优秀的保存下来
+def select(pop, fitness):
     idx = np.random.choice(np.arange(POP_SIZE), size=POP_SIZE, replace=True,
                            p=(fitness)/(fitness.sum()) )
     return pop[idx]
